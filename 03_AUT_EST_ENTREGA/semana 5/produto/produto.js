0b0cd89f95376bdefef372e7bc0baa195e9aee00
yasminroch <99210868+yasminroch@users.noreply.github.com>
2022-05-28 22:35:38 -0300

    Semana 5
    aqui está as atividades da semana 5

---
+++ 03_AUT_EST_ENTREGA/semana 5/produto/produto.js
@@ -0,0 +1,48 @@
+let valor = prompt("informe o preço do produto")
+
+let resto = Number(valor)
+
+while(resto >= 100){
+  document.write('R$100<br>')
+  resto = resto-100
+}
+
+while(resto >= 50){
+  document.write('R$50<br>')
+  resto = resto-50
+}
+
+while(resto >= 20){
+  document.write('R$20<br>')
+  resto = resto-20
+}
+
+while(resto >= 50){
+  document.write('R$50<br>')
+  resto = resto-50
+}
+
+while(resto >= 20){
+  document.write('R$20<br>')
+  resto = resto-20
+}
+
+while(resto >= 10){
+  document.write('R$10<br>')
+  resto = resto-10
+}
+
+while(resto >= 5){
+  document.write('R$5<br>')
+  resto = resto-5
+}
+
+while(resto >= 2){
+  document.write('R$2<br>')
+  resto = resto-2
+}
+
+while(resto >= 1){
+  document.write('R$1<br>')
+  resto = resto-1 
+}
